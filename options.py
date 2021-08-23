58be4c1e230dc6559cc788f0bcb2a1fcd4b1a4b8
newboy <37531872+hfutyanhuan@users.noreply.github.com>
2021-08-23 15:45:55 +0800

    Update options.py

--- options.py
+++ options.py
@@ -15,15 +15,12 @@ class Options():
         parser.add_argument('--no_shuffle', action='store_true', default=True, help='Whether to disturb the data set')
         parser.add_argument('--save_model', action='store_true', default=False, help = 'For Saving the current Model')
         parser.add_argument('--model', type=str, default='resnet18', help='The model to be trained')
-        parser.add_argument('--img_size', type=int, default=112, help='img_size')
+        parser.add_argument('--img_size', type=int, default=224, help='img_size')
         parser.add_argument('--best_acc', type=int, default=20, help='best_acc')
         parser.add_argument('--num_workers', type=int, default=1, help='num_workers')
-        parser.add_argument('--dataset', type=str, default='raf', help='dataset-baseline-dataset')
-        parser.add_argument('--Resume_Model', type=str, help='Resume_Model', default='E:\yanhuan\AGRA_raw\pretrained-model\preTrainedModel_AGRA_2020\ir18_lfw_112_onlyGlobal.pkl')
+        parser.add_argument('--dataset', type=str, default='', help='dataset-baseline-dataset')
+   
 
         args = parser.parse_args()
 
         return args
-
-#NJU3DFE  batch_size 20 lr=0.001
-#Bosphorus batch_size 5 lr=0.001
